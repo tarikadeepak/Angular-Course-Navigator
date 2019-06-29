e7ea73f81caf7393c688a2cc61328b365ac10706
tarikadeepak <tarikadeepak@gmail.com>
2019-06-29 09:55:01 +0530

    Added delete course functionality

--- src/app/course-navigator/course-navigator.component.ts
+++ src/app/course-navigator/course-navigator.component.ts
@@ -12,13 +12,46 @@ export class CourseNavigatorComponent implements OnInit {
 
   courses = []
   modules = []
+  lessons = []
   selectedCourse = {}
+  selectedModule = {}
 
-  selectCourse = course => {
-    this.selectedCourse = course;
+  selectCourse = ($event, course) => {
+    if ($event.target.name === 'deleteButton') {
+      $event.preventDefault();
+      $event.stopPropagation();
+    } else {
+      this.selectedCourse = course;
+      this.lessons = [];
+      this.service
+        .findModulesForCourse(course.id)
+        .then(modules => this.modules = modules);
+    }
+  }
+
+  selectModule = module => {
+    this.selectedModule = module;
     this.service
-      .findModulesForCourse(course.id)
-      .then(modules => this.modules = modules);
+      .findLessonsForModule(module.id)
+      .then(lessons => this.lessons = lessons);
+  }
+
+  deleteCourse = course => {
+    console.log('courses before delete ' , this.courses);
+    this.selectedCourse = {};
+    this.service
+      .deleteCourse(course.id)
+    const index = this.courses.indexOf(course);
+    if (index !== -1) {
+        this.courses.splice(index, 1);
+    }
+    console.log('courses after delete ' , this.courses);
+  }
+
+  addCourse = course => {
+    console.log('Add Course Title ' , course);
+    this.service
+      .createCourse(course);
   }
 
   ngOnInit() {
--- src/app/services/CourseNavigatorService.ts
+++ src/app/services/CourseNavigatorService.ts
@@ -1,3 +1,5 @@
+import {parseHttpResponse} from 'selenium-webdriver/http';
+
 export class CourseNavigatorService{
   findAllCourses = () =>
     fetch
@@ -10,4 +12,36 @@ export class CourseNavigatorService{
     (`http://localhost:8080/api/course/${courseId}/module`)
       .then(response => response.json()
     )
+
+  findLessonsForModule = moduleId =>
+    fetch
+    (`http://localhost:8080/api/course/module/${moduleId}/lesson`)
+      .then(response => response.json()
+    )
+
+  deleteCourse(courseId) {
+    const url = 'http://localhost:8080/api/course'.concat('/').concat(courseId);
+    return fetch(url, {
+      headers: {
+        'Content-Type' : 'application/json'
+      },
+      method: 'DELETE'
+    });
+  }
+
+  createCourse(courseTitle) {
+    const course = {
+      courseTitle: ''
+    }
+    course.courseTitle = courseTitle;
+    console.log('Course while adding ', JSON.stringify(course))
+    return fetch('http://localhost:8080/api/course', {
+      body: JSON.stringify(course),
+      headers: {
+        'Content-Type' : 'application/json'
+      },
+      method: 'POST'
+
+    }).then (response => console.log(response.json()))
+  }
 }
